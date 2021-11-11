43892b25472a7d08629d16ae02c6ac552de0d87b
plateauh <70834431+plateauh@users.noreply.github.com>
2021-11-11 14:19:42 +0300

    internet connection

--- app/src/main/java/com/najed/najed/APIFragment.kt
+++ app/src/main/java/com/najed/najed/APIFragment.kt
@@ -1,13 +1,16 @@
 package com.najed.najed
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -35,19 +38,31 @@ class APIFragment : Fragment() {
         })
 
         showsRecyclerView = apiView.findViewById(R.id.api_shows_rv)
-        showsAdapter = SimpleAdapter(this)
-        showsRecyclerView.adapter = showsAdapter
-        showsRecyclerView.layoutManager = LinearLayoutManager(apiView.context)
-
-        queryEditText = apiView.findViewById(R.id.search_et)
-        searchButton = apiView.findViewById(R.id.search_btn)
-        searchButton.setOnClickListener {
-            if (queryEditText.text.isNotEmpty()) {
-                viewModel.setDataFromAPI(queryEditText.text.toString())
+        val linearLayout = apiView.findViewById<LinearLayout>(R.id.et_and_btn_ll)
+        val connectionTextView = apiView.findViewById<TextView>(R.id.no_connection_tv)
+        if (isConnectedToInternet()){
+            connectionTextView.visibility = INVISIBLE
+            showsRecyclerView.visibility = VISIBLE
+            showsAdapter = SimpleAdapter(this)
+            showsRecyclerView.adapter = showsAdapter
+            showsRecyclerView.layoutManager = LinearLayoutManager(apiView.context)
+            linearLayout.visibility = VISIBLE
+            queryEditText = apiView.findViewById(R.id.search_et)
+            searchButton = apiView.findViewById(R.id.search_btn)
+            searchButton.setOnClickListener {
+                if (queryEditText.text.isNotEmpty()) {
+                    viewModel.setDataFromAPI(queryEditText.text.toString())
+                }
+                else
+                    Toast.makeText(apiView.context, "Please enter something", Toast.LENGTH_SHORT).show()
             }
-            else
-                Toast.makeText(apiView.context, "Please enter something", Toast.LENGTH_SHORT).show()
         }
+        else {
+            showsRecyclerView.visibility = INVISIBLE
+            linearLayout.visibility = INVISIBLE
+            connectionTextView.visibility = VISIBLE
+        }
+
         return apiView
     }
 
@@ -55,4 +70,9 @@ class APIFragment : Fragment() {
         viewModel.addShow(show)
     }
 
+    private fun isConnectedToInternet(): Boolean {
+        val cm = this.context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true
+    }
 }
